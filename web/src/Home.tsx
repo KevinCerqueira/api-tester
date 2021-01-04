c6111f9ed74e7eb5a4d27d3bc4a4c25518959f48
Kevin Gomes <38994372+Goomez@users.noreply.github.com>
2021-01-04 11:16:33 -0300

    :tada: Initial commit

---
+++ web/src/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="app">
+      <div className="container-fluid p-5 text-center">
+        <p className="h1 font-weight-bold text-white">API TESTER</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;

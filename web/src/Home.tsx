f777b02c4d79775a6751af089544ba528578d2c3
Kevin Cerqueira <knbassbr@gmail.com>
2021-01-19 17:39:30 -0300

    :sparkles: added status and response section

--- web/src/Home.tsx
+++ web/src/Home.tsx
@@ -1,19 +1,28 @@
 /* eslint-disable prettier/prettier */
-import React, { useState, FormEvent, ChangeEvent } from 'react';
+import React, { useEffect, useState, FormEvent, ChangeEvent } from 'react';
 import {
   FormControl,
   Button,
   InputLabel,
   Select,
   TextField,
+  CircularProgress,
+  Chip,
 } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
+import axios from 'axios';
 import MyModal from './components/MyModal';
 import './Home.css';
 
-interface inputSubmit {
+interface InputSubmit {
   url: string;
   type: string;
+}
+
+interface Response {
+  data: Array<string>;
+  status: number;
+  statusText: string;
 }
 // interface dataSubmit {
 //   name: string;
@@ -21,10 +30,19 @@ interface inputSubmit {
 // }
 
 function App(): JSX.Element {
-  // const [response, setResponse] = useState();
-  const [state, setState] = useState<inputSubmit>({
+  const [dataResponse, setDataResponse] = useState<Response>({
+    data: [],
+    status: 0,
+    statusText: '',
+  });
+  const [statusTransaction, setStatusTransaction] = useState(false);
+  const [state, setState] = useState<InputSubmit>({
     url: '',
     type: '',
+  });
+  const [statusStyle, setStatusStyle] = useState({
+    color: '#FFF',
+    borderColor: '#FFF',
   });
   const handleChange: any = (
     event: ChangeEvent<{ name?: string; value: unknown }>,
@@ -35,10 +53,59 @@ function App(): JSX.Element {
       [name]: event.target.value,
     });
   };
+  useEffect(() => {
+    if (String(dataResponse.statusText) === '') {
+      setStatusStyle({
+        color: '#FFF',
+        borderColor: '#FFF',
+      });
+    } else if (String(dataResponse.statusText) !== 'OK') {
+      setStatusStyle({
+        color: '#E83B42',
+        borderColor: '#E83B42',
+      });
+    } else {
+      setStatusStyle({
+        color: '#33ff00',
+        borderColor: '#33ff00',
+      });
+    }
+  }, [dataResponse.statusText, statusStyle]);
   async function handleSumbit(event: FormEvent): Promise<void> {
     event.preventDefault();
-
-    console.log(state);
+    setStatusTransaction(true);
+    const { url, type } = state;
+    if (type === '') {
+      alert('Selecione um tipo de envio!');
+    }
+    if (url === '') {
+      alert('Insira uma API!');
+    }
+    if (type === 'GET') {
+      try {
+        await axios.get(url).then((response: any) => {
+          setDataResponse(response);
+        });
+      } catch (error) {
+        const errorRequest = String(error).substring(7);
+        if (errorRequest !== 'Network Error') {
+          const typeError = errorRequest.substring(0, errorRequest.length - 21);
+          const codeError = errorRequest.substring(errorRequest.length - 3);
+          setDataResponse({
+            data: [],
+            status: Number(codeError),
+            statusText: typeError,
+          });
+        } else {
+          setDataResponse({
+            data: [],
+            status: 400,
+            statusText: errorRequest,
+          });
+        }
+      }
+    }
+    setStatusTransaction(false);
   }
   return (
     <div className="app">
@@ -61,54 +128,110 @@ function App(): JSX.Element {
           </a>
         </p>
       </div>
-      <div id="section-input">
-        <form onSubmit={handleSumbit} noValidate autoComplete="off">
-          <div className="flex-container">
-            <div className="flex-item flex-grow-0">
-              <FormControl id="form-control" variant="outlined">
-                <InputLabel htmlFor="select-type">Type</InputLabel>
-                <Select
-                  native
-                  id="select-type"
-                  value={state.type}
-                  onChange={handleChange}
-                  label="Type"
+      <div className="sections flex-container">
+        <div id="section-input" className="flex-item">
+          <form onSubmit={handleSumbit} noValidate autoComplete="off">
+            <div className="flex-container">
+              <div className="flex-item flex-grow-0">
+                <FormControl id="form-control" variant="outlined">
+                  <InputLabel htmlFor="select-type">Type</InputLabel>
+                  <Select
+                    native
+                    id="select-type"
+                    value={state.type}
+                    onChange={handleChange}
+                    label="Type"
+                    inputProps={{
+                      name: 'type',
+                      id: 'select-type',
+                    }}
+                    disabled={statusTransaction}
+                  >
+                    <option value=""> </option>
+                    <option value="GET">GET</option>
+                    <option disabled value="POST" className="text-secondary">
+                      POST
+                    </option>
+                  </Select>
+                </FormControl>
+              </div>
+              <div className="flex-item flex-grow-2">
+                <TextField
+                  id="input-url"
+                  className="w-100"
+                  label="API"
+                  variant="outlined"
                   inputProps={{
-                    name: 'type',
-                    id: 'select-type',
+                    name: 'url',
+                    id: 'input-url',
                   }}
+                  disabled={statusTransaction}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex-item flex-grow-0">
+                <Button
+                  id="btn-send"
+                  className="w-100 h-100"
+                  variant="contained"
+                  color="default"
+                  type="submit"
+                  onClick={handleSumbit}
+                  disabled={statusTransaction}
                 >
-                  <option value=""> </option>
-                  <option value="GET">GET</option>
-                  <option value="POST">POST</option>
-                </Select>
-              </FormControl>
+                  {statusTransaction ? (
+                    <CircularProgress className="circular-progress" />
+                  ) : (
+                    <SendIcon />
+                  )}
+                </Button>
+              </div>
             </div>
-            <div className="flex-item flex-grow-2">
+          </form>
+        </div>
+        <div id="section-response" className="flex-item">
+          <div id="section-response-container" className="flex-container">
+            <div id="section-status" className="flex-item">
+              <div id="container-chips" className="flex-container">
+                <div className="flex-item text-center">
+                  <Chip
+                    style={statusStyle}
+                    className="chip-status"
+                    label={`Status: ${
+                      dataResponse.statusText === ''
+                        ? 'NO DATA'
+                        : dataResponse.status
+                    }`}
+                    variant="outlined"
+                  />
+                </div>
+                <div className="flex-item text-center">
+                  <Chip
+                    style={statusStyle}
+                    className="chip-status"
+                    label={`Status Text: ${
+                      dataResponse.statusText === ''
+                        ? 'NO DATA'
+                        : dataResponse.statusText
+                    }`}
+                    variant="outlined"
+                  />
+                </div>
+              </div>
+            </div>
+            <div id="section-return" className="flex-item">
               <TextField
-                id="input-url"
-                className="w-100"
-                label="API"
+                className="btn-block"
+                id="outlined-multiline-static"
+                label="Return"
+                multiline
+                rows={4}
+                value={dataResponse.data}
                 variant="outlined"
-                inputProps={{
-                  name: 'url',
-                  id: 'input-url',
-                }}
-                onChange={handleChange}
               />
             </div>
-            <div className="flex-item flex-grow-0">
-              <Button
-                id="btn-send"
-                className="w-100 h-100"
-                variant="contained"
-                color="default"
-              >
-                <SendIcon />
-              </Button>
-            </div>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
